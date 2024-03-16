e24a1351a8a77d2ac2d210fb41a7b075369219b2
vasanthakumar45 <vasanthvicky45@gmail.com>
2024-03-16 22:18:02 +0530

    added text to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-# Check if an argument is provided
 if [ -z "$1" ]; then
     echo "Usage: $0 <target>"
     echo "Specify 'dev' or 'prod' as the target."
     exit 1
 fi
 
-# Check the target argument
+
 if [ "$1" == "dev" ]; then
-    # Tag the built image for the dev repository on Docker Hub
+    
     docker tag "$(docker images -q | head -n 1)" vasanth45/dev_repo:tagname
     
     docker push vasanth45/dev_repo:tagname
     
     docker run -d -p 80:80 vasanth45/dev_repo:tagname
 elif [ "$1" == "prod" ]; then
-    # Tag the built image for the prod repository on Docker Hub
+    
     docker tag "$(docker images -q | head -n 1)" vasanth45/prod_repo:tagname
     
     docker push vasanth45/prod_repo:tagname
